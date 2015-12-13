021e681ce7e4eac9b71d9462b4cac21cc4ffff13
Rein Henrichs <rein.henrichs@gmail.com>
2015-12-13 14:40:35 -0800

    Add Conversion wip

---
+++ src/Hexmap/Coordinates/Conversion.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Hexmap.Coordinates.Conversion where
+
+import           Control.Lens.Iso
+import           Hexmap.Coordinates.Axial
+import           Hexmap.Coordinates.Offset
+import           Hexmap.Coordinates.Pixel
+import           Linear
+
+class Conversion a b where
+  conversion :: Iso' a b
+
+instance Integral a => Conversion (Offset a) (Axial a) where
+  conversion = iso toAxial toOffset
+  where
+      toAxial (Offset (V2 c r)) = Axial (V2 (c - (r `div` 2)) r)
+      toOffset (Axial (V2 c r)) = Offset (V2 (c + (r `div` 2)) r)
